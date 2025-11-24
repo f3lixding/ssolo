df76d50cbe81dda0ae9ae928a7cf42fc53b00e7f
f3lixding <felixding1989@gmail.com>
2025-11-23 21:45:57 -0800

    fixes build script for macos

--- build.zig
+++ build.zig
@@ -68,11 +68,13 @@ pub fn build(b: *std.Build) void {
     bin_to_add.root_module.addImport("zigimg", zigimg_dep.module("zigimg"));
     bin_to_add.root_module.addImport("pda", pda_dep.module("pda"));
 
-    bin_to_add.linkSystemLibrary("GL");
-    bin_to_add.linkSystemLibrary("X11");
-    bin_to_add.linkSystemLibrary("Xi");
-    bin_to_add.linkSystemLibrary("Xcursor");
-    bin_to_add.linkSystemLibrary("asound");
+    if (target.result.os.tag != .macos) {
+        bin_to_add.linkSystemLibrary("GL");
+        bin_to_add.linkSystemLibrary("X11");
+        bin_to_add.linkSystemLibrary("Xi");
+        bin_to_add.linkSystemLibrary("Xcursor");
+        bin_to_add.linkSystemLibrary("asound");
+    }
 
     if (!no_bin) {
         b.installArtifact(bin_to_add);
