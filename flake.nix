59bc838a32d19472bec2227325bede0e26a8f74b
Felix Ding <felixding1989@gmail.com>
2025-11-27 10:32:54 -0800

    adds nix build description

--- flake.nix
+++ flake.nix
@@ -40,19 +40,35 @@
           alsa-lib
         ];
       in {
+        # Can't do this for now. The Zig build system would try to do the following, which are forbidden by the nix sandbox:
+        # - Download packages from build.zig.zon (no network access)
+        # - Write it to global zig cache (no access to such a dir)
+        # In order for this to work properly, we would need to prefetch dependencies.
+        # There are tools out there that does this (e.g. zig2nix). But I haven't figured out how to handle transitive dependency with it yet.
+        #
+        # That is with the exception of disabling sandbox i.e. nix build --option sandbox false
+        # though you do need privlege escalation so you would need to run it iwth sudo i.e. sudo nix build --option sandbox false
         packages.default = pkgs.stdenv.mkDerivation {
           name = "ssolo";
           src = ./.;
 
           nativeBuildInputs = buildInputs;
 
+          preBuild = ''
+            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+          '';
+
           buildPhase = ''
+            runHook preBuild
             zig build -Doptimize=ReleaseSafe
+            runHook postBuild
           '';
 
           installPhase = ''
+            runHook preInstall
             mkdir -p $out/bin
             cp zig-out/bin/* $out/bin/
+            runHook postInstall
           '';
         };
 
